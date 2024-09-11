d82715f9bf976fea0384d022222343e3ecda1156
s2quake <han0210@netsgo.com>
2024-09-11 13:37:27 +0900

    feat: Configure solution to build and run project

---
+++ README.md
@@ -0,0 +1,11 @@
+# 9c Node
+
+This project is a blockchain node for `NineChronicles` implemented using the [Libplanet.Node SDK] and
+[Lib9c]
+
+
+[Libplanet.Node SDK]: https://github.com/planetarium/libplanet/tree/exp/sdk/node
+[Lib9c]: https://github.com/planetarium/lib9c/tree/development
+
+
+WIP
---
+++ src/NineChronicles.Node.Executable/Program.cs
@@ -0,0 +1,36 @@
+﻿using Libplanet.Node.Extensions;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Logging.AddConsole();
+if (builder.Environment.IsDevelopment())
+{
+    builder.WebHost.ConfigureKestrel(options =>
+    {
+        // Setup a HTTP/2 endpoint without TLS.
+        options.ListenLocalhost(5259, o => o.Protocols =
+            HttpProtocols.Http1AndHttp2);
+        options.ListenLocalhost(5260, o => o.Protocols =
+            HttpProtocols.Http2);
+    });
+
+    builder.Services.AddEndpointsApiExplorer();
+}
+
+builder.Services.AddGrpc();
+builder.Services.AddLibplanetNode(builder.Configuration);
+
+var handlerMessage = """
+    Communication with gRPC endpoints must be made through a gRPC client. To learn how to
+    create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909
+    """;
+using var app = builder.Build();
+
+app.MapGet("/", () => handlerMessage);
+if (builder.Environment.IsDevelopment())
+{
+    app.MapSchemaBuilder("/v1/schema");
+    app.MapGet("/schema", context => Task.Run(() => context.Response.Redirect("/v1/schema")));
+}
+
+await app.RunAsync();
